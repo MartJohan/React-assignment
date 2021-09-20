b951665cef3ec1ea9328c7e04cff6cf873634b03
Martin Johansen <Martin.Johansen@no.experis.com>
2021-09-20 15:52:13 +0200

    Added some small styling

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,6 +1,11 @@
 function Input() {
     return (
-        <div></div>
+    <div className="input-group mb-3 w-100">
+        <input type="text" className="form-control" aria-label="" aria-describedby="basic-addon" placeholder="What's your name?"></input>
+        <div className="input-group-append">
+            <button className="btn btn-outline-secondary" type="button">Go</button>
+        </div>
+    </div>
     )
 }
 
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,6 +1,17 @@
+import Input from './Input';
+
 function Login() {
     return (
-        <div> Login</div>
+        <div> 
+            <div>
+                <h1>Lost in Translation</h1>
+                <h3>Get Started</h3>
+            </div>
+            <div className="col-md-8 offset-md-2">
+                <Input/>
+            </div>
+
+        </div>
     )
 }
 
