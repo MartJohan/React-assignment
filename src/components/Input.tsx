9b6623842799a62b7ede0691363e7faed153128c
Martin Johansen <Martin.Johansen@no.experis.com>
2021-09-21 10:31:40 +0200

    Commit before pull

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,7 +3,7 @@ function Input() {
     <div className="input-group mb-3 w-100">
         <input type="text" className="form-control" aria-label="" aria-describedby="basic-addon" placeholder="What's your name?"></input>
         <div className="input-group-append">
-            <button className="btn btn-outline-secondary" type="button">Go</button>
+            <button className="btn btn-primary" type="button">{ "->" }</button>
         </div>
     </div>
     )
